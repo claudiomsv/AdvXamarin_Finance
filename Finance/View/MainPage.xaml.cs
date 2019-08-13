1ce6c359065ff599a23ad0fd6da28e9bd1432a2e
Claudio Vicente <claudio.vicente@serpro.gov.br>
2019-08-13 17:56:12 -0300

    AdvXamarin initial

---
+++ Finance/View/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Finance.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //IOS IMPORTANT
+            //IPHONE X / Xs / Xs Max
+            //Inibe que o conteudo invada a barra inferior (que controla navegação nos dispositivos acima)
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+    }
+}
