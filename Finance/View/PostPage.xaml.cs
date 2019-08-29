258405c2290a08f09a0d009bfb797ee6a6e690ab
Claudio Vicente <claudio.vicente@serpro.gov.br>
2019-08-29 09:51:50 -0300

    New features test - Lesson 44

--- Finance/View/PostPage.xaml.cs
+++ Finance/View/PostPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Finance.Model;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace Finance.View
@@ -18,7 +20,40 @@ namespace Finance.View
             InitializeComponent();
             Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
 
-            webView.Source = item.ItemLink;
+            try
+            {
+                //throw (new Exception("Excecao de teste para appcenter crashes"));
+                webView.Source = item.ItemLink;
+                var properties = new Dictionary<string, string>
+                {
+                    {"Blog_Post",$"{item.Title}"}
+                };
+                TrackEvent(properties);
+            }
+            catch(Exception ex)
+            {
+                var properties = new Dictionary<string, string>
+                {
+                    {"Blog_Post",$"{item.Title}"}
+                };
+                
+                TrackError(ex, properties);
+            }
+        }
+
+        private async void TrackEvent(Dictionary<string, string> properties)
+        {
+            if(await Analytics.IsEnabledAsync())
+                Analytics.TrackEvent("Blog_post_openned", properties);
+        }
+
+        private async void TrackError(Exception ex, Dictionary<string, string> properties)
+        {
+            if (await Crashes.IsEnabledAsync())
+            {
+                // MASTERCLASS - PODE RECEBER DICIONARIO DE STRING
+                Crashes.TrackError(ex, properties);
+            }
         }
     }
 }
